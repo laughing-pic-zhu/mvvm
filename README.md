86fdcb2617f7f3f1e2deb97bab9eb2f9da8217ee
laughing-zhu <wb-zhujian.f@alibaba-inc.com>
2016-09-28 11:06:17 +0800

    add v-model

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # 简洁mvvm框架 
 laugh提供简洁的双向绑定功能
-## 指令
-v-bind : 双向数据绑定
-v-text : 单项数据绑定
-v-show : 是否显示
+ - 指令
+    - v-model : 双向数据绑定
+    - v-text : 单项数据绑定
+    - v-show : 是否显示
+ - $watch
+    ```
+    var cc={
+        title: '欢迎来到英雄联盟',
+        show: false
+    };
+    var vm=new laugh({
+        el:'#person',
+        data:obj
+    });
+    vm.$watch(cc,function(path,oldValue,newValue){
+        console.log(path);
+        console.log(oldValue);
+        console.log(newValue);
+    })
+    ```
--- src/laugh.js
+++ src/laugh.js
@@ -1,4 +1,6 @@
-function laughing(obj, nodes) {
+function laugh(params) {
+    var nodes = document.querySelector(params.el);
+    var obj = params.data||{};
     this.cache = [];
 
     this.compile = function (dom) {
@@ -25,19 +27,29 @@ function laughing(obj, nodes) {
     this.paserNode = function (node) {
         var text = node.getAttribute('v-text');
         var show = node.getAttribute('v-show');
-        var bind = node.getAttribute('v-bind');
+        //var bind = node.getAttribute('v-bind');
+        var model = node.getAttribute('v-model');
+
         var temp = {
             node: node
         };
+
         if (text) {
-            temp.text=text;
+            temp.text = text;
         }
         if (show) {
-            temp.show=show;
+            temp.show = show;
         }
-        if (bind) {
-            temp.bind=bind;
-            node.addEventListener('input', this.onchange.bind(this, bind), false);
+        //if (bind) {
+        //    temp.bind = bind;
+        //    node.addEventListener('input', this.onchange.bind(this, bind), false);
+        //}
+        if (model) {
+            if(!obj.hasOwnProperty(model)){
+                obj[model]='';
+            }
+            temp.model = model;
+            node.addEventListener('input', this.onchange.bind(this, model), false);
         }
         return temp;
     };
@@ -50,7 +62,7 @@ function laughing(obj, nodes) {
         if (typeof content == 'function') {
             content = content.apply(obj);
         }
-        node.textContent = content;
+        node.textContent = content||'';
     };
 
     this.styleChange = function (node, key, value) {
@@ -58,11 +70,11 @@ function laughing(obj, nodes) {
     };
 
     this.valueChange = function (node, content) {
-        node.value = content;
+        node.value = content||'';
     };
 
-    this.judgeNull=function(value){
-        if(value===undefined||value===null||value===''){
+    this.judgeNull = function (value) {
+        if (value === undefined || value === null || value === '') {
             return false;
         }
         return true;
@@ -83,14 +95,68 @@ function laughing(obj, nodes) {
                 }
                 this.styleChange(item.node, 'display', value);
             }
-            if (this.judgeNull(item.bind)) {
-                this.valueChange(item.node, obj[item.bind]);
+            if (this.judgeNull(item.model)) {
+                this.valueChange(item.node, obj[item.model]);
             }
         }, this);
     };
 
-    this.observe=function(){
-        new Watch(obj,this.render.bind(this));
+    this.$watch = function (obj, callback) {
+        this.callback = callback;
+        this.$observe = function (_obj, path) {
+            var type = Object.prototype.toString.call(_obj);
+            if (type == '[object Object]' || type == '[object Array]') {
+                this.$observeObj(_obj, path);
+                if (type == '[object Array]') {
+                    this.cloneArray(_obj, path);
+                }
+            }
+        };
+
+        this.$observeObj = function (obj, path) {
+            var t = this;
+            Object.keys(obj).forEach(function (prop) {
+                var val = obj[prop];
+                var tpath = path.slice(0);
+                tpath.push(prop);
+                Object.defineProperty(obj, prop, {
+                    get: function () {
+                        return val;
+                    },
+                    set: function (newVal) {
+                        var temp = val;
+                        val = newVal;
+                        t.callback(tpath, newVal, temp);
+                    }
+                });
+                t.$observe(val, tpath);
+            });
+        };
+
+        this.cloneArray = function (a_array, path) {
+            var ORP = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+            var arrayProto = Array.prototype;
+            var newProto = Object.create(arrayProto);
+            var t = this;
+            ORP.forEach(function (prop) {
+                Object.defineProperty(newProto, prop, {
+                    value: function (newVal) {
+                        arrayProto[prop].apply(a_array, arguments);
+                        t.callback(path, newVal);
+                    },
+                    enumerable: false,
+                    configurable: true,
+                    writable: true
+                });
+            });
+            a_array.__proto__ = newProto;
+        };
+
+        this.$observe(obj, []);
+    };
+
+    this.observe = function () {
+        this.$watch(obj, this.render.bind(this));
     };
 
     this.init = function () {
@@ -103,70 +169,7 @@ function laughing(obj, nodes) {
     this.init();
 }
 
-/**
- *
- * @param obj 需要监听的对象或数组
- * @param callback 当对应属性变化的时候触发的回调函数
- * @constructor
- */
-function Watch(obj, callback) {
-    this.callback = callback;
-    //监听_obj对象 判断是否为对象,如果是数组,则对数组对应的原型进行封装
-    //path代表相应属性在原始对象的位置,以数组表示. 如[ 'a', 'dd', 'ddd' ] 表示对象obj.a.dd.ddd的属性改变
-    this.observe = function (_obj, path) {
-        var type=Object.prototype.toString.call(_obj);
-        if (type== '[object Object]'||type== '[object Array]') {
-            this.observeObj(_obj, path);
-            if (type == '[object Array]') {
-                this.cloneArray(_obj, path);
-            }
-        }
-    };
 
-    //遍历对象obj,设置set,get属性,set属性能触发callback函数,并将val的值改为newVal
-    //遍历结束后再次调用observe函数 判断val是否为对象,如果是则在对val进行遍历设置set,get
-    this.observeObj = function (obj, path) {
-        var t = this;
-        Object.keys(obj).forEach(function (prop) {
-            var val = obj[prop];
-            var tpath = path.slice(0);
-            tpath.push(prop);
-            Object.defineProperty(obj, prop, {
-                get: function () {
-                    return val;
-                },
-                set: function (newVal) {
-                    val = newVal;
-                    t.callback(tpath, newVal, val);
-                }
-            });
-            t.observe(val, tpath);
-        });
-    };
-
-    //通过对特定数组的原型中间放一个newProto原型,该原型继承于Array的原型,但是对push,pop等数组操作属性进行封装
-    this.cloneArray = function (a_array,path) {
-        var ORP = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
-        var arrayProto = Array.prototype;
-        var newProto = Object.create(arrayProto);
-        var t = this;
-        ORP.forEach(function (prop) {
-            Object.defineProperty(newProto, prop, {
-                value: function (newVal) {
-                    arrayProto[prop].apply(a_array, arguments);
-                    t.callback(path, newVal);
-                },
-                enumerable: false,
-                configurable: true,
-                writable: true
-            });
-        });
-        a_array.__proto__ = newProto;
-    };
-
-    //开始监听obj对象,初始path为[]
-    this.observe(obj, []);
-}
 
 
 
